256599e5095bb71ce5a1fa48b44005f119be455f
Tuan Pham <tuanpham.developer@gmail.com>
2020-06-24 12:35:15 -0400

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Tuanp Dev`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Tuan Pham`,
+      summary: `who lives and works in Connecticut building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `Where you can learn and play.`,
+    siteUrl: `https://tuanp.dev/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `tuanp.dev`,
     },
   },
   plugins: [
@@ -60,8 +60,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Tuanp Dev`,
+        short_name: `tuanp.dev`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
