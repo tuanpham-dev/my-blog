3c9796c12e272244c9cc17ae90989279e030f07d
Tuan Pham <tuanpham.developer@gmail.com>
2020-06-24 19:37:31 -0400

    Make footer always be bottom of document view

--- src/components/layout.js
+++ src/components/layout.js
@@ -54,11 +54,14 @@ const Layout = ({ location, title, children }) => {
         marginRight: `auto`,
         maxWidth: rhythm(24),
         padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        display: `flex`,
+        flexDirection: `column`,
+        minHeight: `100vh`,
       }}
     >
       <header>{header}</header>
       <main>{children}</main>
-      <footer>
+      <footer style={{ marginTop: `auto` }}>
         © {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.org">Gatsby</a>
